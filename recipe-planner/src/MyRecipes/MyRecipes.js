0809521dec8c511af08fe92b44046ab95ec44992
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2023-12-05 09:58:36 -0800

    MyRecipes finally works

--- recipe-planner/src/MyRecipes/MyRecipes.js
+++ recipe-planner/src/MyRecipes/MyRecipes.js
@@ -10,71 +10,40 @@ import { getSavedRecipes } from "../Data/624API";
 import { getRecipeDetails } from "../Data/Spoonacular";
 import uuidV4 from "uuid/v4";
 
-const MyRecipes = ({ navigation, params }) => {
+const MyRecipes = ({ navigation }) => {
+  const [loading, setLoading] = useState(true);
   const [myRecipes, setMyRecipes] = useState([]);
-  const [myRecipeIds, setMyRecipeIds] = useState([]);
-
-  const fetchSavedRecipeIds = async () => {
-    try {
-      const savedRecipeIds = await getSavedRecipes();
-      console.log("Fetched saved recipe IDs:", savedRecipeIds);
-      setMyRecipeIds(savedRecipeIds);
-    } catch (error) {
-      console.error("Failed to fetch saved recipe IDs:", error);
-    }
-  };
-
-  const fetchSavedRecipes = async () => {
-    try {
-      console.log("Fetching recipes for IDs:", myRecipeIds);
-      for (const id of myRecipeIds) {
-        const recipe = await getRecipeDetails(id);
-        console.log("Fetched recipe:", recipe);
-        setMyRecipes((prevRecipes) => [...prevRecipes, recipe]);
-      }
-    } catch (error) {
-      console.error("Failed to fetch saved recipes:", error);
-    }
-  };
 
   useEffect(() => {
-    fetchSavedRecipeIds();
+    async function loadAllRecipes() {
+      try {
+        const savedRecipeIds = await getSavedRecipes();
+        console.log("Fetched saved recipe IDs:", savedRecipeIds);
+
+        if (savedRecipeIds.savedRecipes.length > 0) {
+          const recipes = await Promise.all(
+            savedRecipeIds.savedRecipes.map((id) => getRecipeDetails(id))
+          );
+
+          setMyRecipes(recipes);
+        }
+      } catch (error) {
+        console.error("Error loading recipes:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadAllRecipes();
   }, []);
 
-  useEffect(() => {
-    console.log("myRecipeIds updated:", myRecipeIds);
-    if (myRecipeIds.length > 0) {
-      fetchSavedRecipes();
-    }
-  }, [myRecipeIds]);
-
-  // const saveToMyRecipes = (recipe) => {
-  //   setMyRecipes((prevRecipes) => [...prevRecipes, recipe]);
-  // };
-
-  // const deleteSavedRecipe = (recipe) => {
-  //   setMyRecipes((prevRecipes) =>
-  //     prevRecipes.filter((item) => item !== recipe)
-  //   );
-  // };
-
-  // const navigate = (item) => {
-  //   props.navigation.navigate("Recipe", { recipe: item });
-  // };
-
-  // useEffect(async () => {
-  //   // Handle loadedRecipes
-  //   const loadedRecipes = await getSavedRecipes();
-  //   if (loadedRecipes) {
-  //     setMyRecipes(loadedRecipes);
-  //   }
-
-  //   // Handle deleteRecipe
-  //   const deleteRecipe = props.route.params?.deleteRecipe;
-  //   if (deleteRecipe) {
-  //     deleteSavedRecipe(deleteRecipe);
-  //   }
-  // }, [props.route.params]);
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.fields}>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView contentContainerStyle={[!myRecipes.length && { flex: 1 }]}>
@@ -86,19 +55,20 @@ const MyRecipes = ({ navigation, params }) => {
           },
         ]}
       >
-        {!myRecipes.length && (
+        {!myRecipes.length ? (
           <Text style={styles.fields}>No saved recipes yet!</Text>
+        ) : (
+          myRecipes.map((item) => (
+            <TouchableWithoutFeedback
+              onPress={() => navigation.navigate("Recipe", { recipe: item })}
+              key={uuidV4()}
+            >
+              <View style={styles.recipeContainer}>
+                <Text style={styles.recipe}>{item.title}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          ))
         )}
-        {myRecipes.map((item, index) => (
-          <TouchableWithoutFeedback
-            onPress={() => navigate(item)}
-            key={uuidV4()}
-          >
-            <View style={styles.recipeContainer}>
-              <Text style={styles.recipe}>{item.title}</Text>
-            </View>
-          </TouchableWithoutFeedback>
-        ))}
       </View>
     </ScrollView>
   );
